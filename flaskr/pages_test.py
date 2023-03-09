7955acfd64be9f382b1653d1dca58c87d641d9c5
Andro Aguilar <andro_aguilar@techexchange.in>
2023-03-09 12:08:05 +0000

    added test cases for pages, routes within pages, and an extra case for home

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -20,28 +20,43 @@ def client(app):
 def test_home_page(client):
     resp = client.get("/")
     assert resp.status_code == 200
-    assert b"asdfadfasdfadfadfadasfads" in resp.data
+    assert b"Welcome to our Wiki!" in resp.get_data()
+    resp = client.get("/home")
+    assert resp.status_code == 200
+    assert b"Welcome to our Wiki!" in resp.get_data()
 
 # TODO(Project 1): Write tests for other routes.
 def test_home_signup(client):
     resp = client.get("/signup")
     assert resp.status_code == 200
-    assert b"Create an account" in resp.data
+    assert b"Create an account" in resp.get_data()
 
 def test_home_login(client):
     resp = client.get("/login")
     assert resp.status_code == 200
-    assert b"Log in" in resp.data
+    assert b"Log in" in resp.get_data()
 
 def test_upload(client):
     resp = client.get("/upload")
     assert resp.status_code == 200
-    assert b"Select a file to upload:" in resp.data
+    assert b"Select a file to upload:" in resp.get_data()
 
 def test_about(client):
     resp = client.get("/about")
     assert resp.status_code == 200
-    assert b"Welcome to our Wiki!" in resp.data
+    assert b"About our Wiki!" in resp.get_data()
+
+def test_wiki_list(client):
+    resp = client.get("/pages")
+    assert resp.status_code == 200
+    assert b"Pages contained in this Wiki" in resp.get_data()
+
+def test_wiki_page(client):
+    resp = client.get("/pages/Super%20Mario%20Kart")
+    assert resp.status_code == 200
+    assert b"Super Mario Kart[a] is a kart racing game developed and published by Nintendo" in resp.get_data()
+
+
 
 
 
