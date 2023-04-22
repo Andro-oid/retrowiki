5bc6c1a943c805694c099b550c68fd0aa4b0f9a1
Manuel Mares <manuelms@techexchange.in>
2023-04-22 18:31:43 +0000

    fixed styles

--- flaskr/wikimusic_test.py
+++ flaskr/wikimusic_test.py
@@ -1,13 +1,16 @@
 from unittest.mock import patch, mock_open, MagicMock
 import pytest
 from .wikimusic import get_wikipedia_articles, get_wikipedia_article, get_iframe_spotify_songs
-    
+
+
 @patch("flaskr.wikimusic.get_wikipedia_article")
 def test_get_wikipedia_articles(mock_get_wikipedia_article):
     mock = MagicMock()
     mock.get.return_value.json.return_value = {
         "query": {
-            "search": [{"title": "new song"}  ]        
+            "search": [{
+                "title": "new song"
+            }]
         }
     }
     mock_get_wikipedia_article.return_value = "new article"
@@ -20,23 +23,23 @@ def test_get_wikipedia_article():
     mock = MagicMock()
     mock.get.return_value.json.return_value = {
         "query": {
-            "pages": 
-            {
-                "articles":
-                    {
-                        "title": "article1",
-                        "extract": "body of article",
-                        "fullurl": "article.com"
-                    }        
+            "pages": {
+                "articles": {
+                    "title": "article1",
+                    "extract": "body of article",
+                    "fullurl": "article.com"
+                }
             }
         }
     }
 
     articles = get_wikipedia_article("black bird", mock)
 
-    assert articles == {'title': 'article1', 'extract': 'body of article', 'fullurl': 'article.com'}
-
-
+    assert articles == {
+        'title': 'article1',
+        'extract': 'body of article',
+        'fullurl': 'article.com'
+    }
 
 
 @patch("flaskr.wikimusic._getToken")
@@ -44,10 +47,8 @@ def test_get_iframe_spotify_songs_case1(mock__getToken):
     mock = MagicMock()
     mock.get.return_value.json.return_value = {
         "tracks": {
-            "items":
-            {
-                0:
-                {
+            "items": {
+                0: {
                     "id": "song1"
                 }
             }
@@ -64,10 +65,8 @@ def test_get_iframe_spotify_songs_case2(mock__getToken):
     mock = MagicMock()
     mock.get.return_value.json.return_value = {
         "tracks": {
-            "items":
-            {
-                0:
-                {
+            "items": {
+                0: {
                     "id": "song1",
                     "id": "song2",
                     "id": "song3",
@@ -80,6 +79,5 @@ def test_get_iframe_spotify_songs_case2(mock__getToken):
     }
 
     iframes = get_iframe_spotify_songs("black bird", "beatles", mock)
-    answer =    '<iframe src="https://open.spotify.com/embed?uri=spotify:track:song6" class="spotify_song" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
+    answer = '<iframe src="https://open.spotify.com/embed?uri=spotify:track:song6" class="spotify_song" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
     assert iframes == answer
-
